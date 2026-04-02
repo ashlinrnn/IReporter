4c72c19fa159eb2256be2cd0254f54618970fb84
Ashlin Rofina <ashlinrofina@gmail.com>
2026-04-02 10:04:27 +0300

    adds table

--- server/models/image/image.py
+++ server/models/image/image.py
@@ -1,2 +1,14 @@
-class Image():
-    pass
+from sqlalchemy_serializer import SerializerMixin
+from sqlalchemy.orm import validates
+from ...config import db 
+
+class Image(db.model, SerializerMixin):
+    
+    __tablenamename__ = "images" 
+
+    id = db.Column(db.Integer, primary_key=True)
+    record_id = db.Column(db.Integer, db.ForeignKey("recors.id"), nullable= False)
+    image_url = db.Column(db.Text, nullable = False)
+    created_at = db.Column(db.DateTime, server_default=db.func.now())
+    
+    
