49cb387d64eaebe62ad1eb837cab90c64efaf7c5
Ashlin Rofina <ashlinrofina@gmail.com>
2026-04-02 13:07:21 +0300

    creates the video model

--- server/models/video/video.py
+++ server/models/video/video.py
@@ -3,5 +3,18 @@ from sqlalchemy.orm import validates
 from ...config import db 
 
 
-class Video(db.Model, serialize_mixin):
-    pass
+class Video(db.Model, SerializerMixin):
+    __tablename__ = 'videos' 
+
+    id = db.Column(db.Integer, primary_key=True)
+    record_id = db.Column(db.Integer, db.ForeignKey("reports.id"), nullable= False)
+    video_url = db.Column(db.Text, nullable = False)
+    created_at = db.Column(db.DateTime, server_default=db.func.now())
+    
+    record = db.relationship('Record', backref=db.backref('videos', lazy=True)) 
+
+    @validates('video_url')
+    def validate_video_url(self, key, value):
+        if not value:
+            raise ValueError('Video URL must be provided')
+        return value
