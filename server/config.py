1ead36edc1118994a78d2ea7964a88bd217e9990
JeffM20501 <kimmuna440@gmail.com>
2026-04-04 16:06:39 +0300

    added password encription

--- server/config.py
+++ server/config.py
@@ -3,6 +3,7 @@ from flask_migrate import Migrate
 from dotenv import load_dotenv
 from flask_sqlalchemy import SQLAlchemy
 from sqlalchemy import MetaData
+from flask_bcrypt import Bcrypt
 
 load_dotenv()
 
@@ -10,7 +11,10 @@ metadata=MetaData()
 db=SQLAlchemy(metadata=metadata)
 migrate=Migrate()
 
+bcrypt=Bcrypt()
+
 def config_app(app):
     app.config.from_prefixed_env()
     db.init_app(app)
     migrate.init_app(app,db)
+    bcrypt.init_app(app)
--- server/models/user/user.py
+++ server/models/user/user.py
@@ -1,6 +1,7 @@
 from sqlalchemy_serializer import SerializerMixin
 from sqlalchemy.orm import validates
-from ...config import db
+from ...config import db, bcrypt
+from sqlalchemy.ext.hybrid import hybrid_property
 
 class User(db.Model, SerializerMixin):
     
@@ -18,6 +19,25 @@ class User(db.Model, SerializerMixin):
     
     records=db.relationship('Record', backref='user', cascade='all, delete-orphan')
     
+    def __init__(self,**kwargs):
+        password=kwargs.pop('password',None)
+        super().__init__(**kwargs)
+        if password:
+            self.password=password
+    
+    @hybrid_property
+    def password(self):
+        raise AttributeError('Can not access Attribute')
+    
+    @password.setter
+    def password(self,password):
+        
+        self.password_hash=bcrypt.generate_password_hash(password.encode('utf-8')).decode('utf-8')
+        
+    def authenticate(self,password):
+        return bcrypt.check_password_hash(self.password_hash, password.encode('utf-8'))
+    
+    
     @validates('username')
     def validate_username(self,key,username):
         if not username:
@@ -33,5 +53,3 @@ class User(db.Model, SerializerMixin):
     
     def __repr__(self):
         return f'<User id:{self.id} name:{self.username} email:{self.email} >'
-    
-    
