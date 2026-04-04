b6e980a228fcf8bfc3a5b6d36f8636ad7a9dc3da
Ashlin Rofina <ashlinrofina@gmail.com>
2026-04-04 14:07:40 +0300

    fixed error in the rotes

--- server/models/image/routes/image_routes.py
+++ server/models/image/routes/image_routes.py
@@ -1,55 +1,56 @@
 from flask import request,g, jsonify, make_response
 from flask_restful import Resource
-from ... import Image
-from ....config import db
-from werkzeug.exceptions import Forbidden
+from server.models.image.image import Image
+from server.config import db
 
 class ImageResource(Resource):
-    def post(self): 
+
+    def post(self):
         data = request.get_json()
 
-        image = Image( 
-            image_url=data['image_url'], 
+        image = Image(
+            image_url=data['image_url'],
             record_id=data['record_id']
-
-        ) 
-        db.sesseion.add(image) 
-        db.session.commit() 
-
-        return make_response(image.to_dict(), 201) 
-
-    def get(self): 
-        response_dict_list = [image.to_dict() for image in Image.query.all()] 
-        return make_response(response_dict_list, 200) 
-
-class ImageByID(Resource):  
-
-    def get(self, id): 
-        response_dict = Image.query.filter_by(id=id).first().to_dict()
-
-        response = make_response(
-            response_dict,
-            200,
         )
 
-        return response 
+        db.session.add(image)  
+        db.session.commit()
 
-    def delete(self, id): 
-        image = Image.query.filter_by(id=id).first()
+        return make_response(image.to_dict(), 201)
+
+
+    def get(self):
+        images = [image.to_dict() for image in Image.query.all()]
+        return make_response(images, 200)
+
+
+class ImageByID(Resource):
+
+    def get(self, id):
+        image = Image.query.get(id)
+
+        if not image:   
+            return make_response({"error": "Image not found"}, 404)
+
+        return make_response(image.to_dict(), 200)
+
+
+    def delete(self, id):
+        image = Image.query.get(id)
 
         if not image:
-            return make_response(jsonify({"error": "Plant not found"}), 404)
-
+            return make_response({"error": "Image not found"}, 404)  
         db.session.delete(image)
         db.session.commit()
 
-        return make_response("", 204)  
+        return make_response("", 204)
+
 
     def patch(self, id):
         image = Image.query.get(id)
 
         if not image:
-            return {"error": "image not found"}, 404
+            return {"error": "Image not found"}, 404
 
         data = request.get_json()
 
