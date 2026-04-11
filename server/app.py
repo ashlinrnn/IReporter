f2bcba4de15548f25dc484dd254920069ef96b0a
Ashlin Rofina <ashlinrofina@gmail.com>
2026-04-11 15:13:26 +0300

    adds cloudinary configuration

--- server/app.py
+++ server/app.py
@@ -4,15 +4,26 @@ from flask_cors import CORS
 from .config import config_app
 from flask_migrate import Migrate
 from  .models import *
-from .routes.create_blueprint import api_pb 
-
-
+from .routes.create_blueprint import api_pb  
+import os 
+import cloudinary 
 
 def create_app(test_config=None):
-    app=Flask(__name__)
+    app=Flask(__name__)  
+     
+    
     if test_config:
         app.config.update(test_config)
-    config_app(app)
+    config_app(app) 
+
+    
+
+    cloudinary.config( 
+        cloud_name=os.getenv("CLOUDINARY_CLOUD_NAME"), 
+        api_key= os.getenv("CLOUDINARY_API_KEY"),
+        api_secret=os.getenv("CLOUDINARY_API_SECRET"),
+        secure=True
+    )
     CORS(app, origins=[
             "http://localhost:5173", #dev
             "https://ireporter-xi.vercel.app" #prod 
