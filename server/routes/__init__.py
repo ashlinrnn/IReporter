132ab05ec48e5fc8f1e15f732bbca35d247d71f4
JeffM20501 <kimmuna440@gmail.com>
2026-04-11 16:49:15 +0300

    updated routes __init__

--- server/routes/__init__.py
+++ server/routes/__init__.py
@@ -2,3 +2,4 @@ from .create_blueprint import api_pb
 from . import create_generic_routes
 from ..models.record.routes import * #import api routes from record models 
 from ..models.user.routes import * 
+from . import media
